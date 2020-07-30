094cc6d5796322b46ab7389b7d69f537092922a2
Daniel Taufiq <Daniel-Taufiq@users.noreply.github.com>
2020-07-30 11:59:25 -0500

    Current Weather & VueJs
    + Added new VueJS Component
    + Added current weather

--- app.js
+++ app.js
@@ -1,39 +1,23 @@
-// libraries
-const express = require('express')
-const fetch = require("node-fetch");
+var app = new Vue({
+    el: '#app',
+    data: {
+        temp: '',
+    },
 
-// app
-const app = express();
-
-const port = 8000;
-
-app.get('/', (req, res) => {
-    res.statusCode = 200
-    res.send('Hello World')
-})
-
-//localhost:8000/users/23/books/3422
-app.get('/users/:userId/books/:bookId', function(req, res) {
-    res.send(req.params)
-    console.log(req.params)
-})
-
-
-let api = 'https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=82d42ea6185f3c0018888ea6bc0444e3';
-
-fetch(api)
-    .then(response => response.json())
-    .then(data => {
-        var location = data['name']
-        var temp = data['main']['temp']
-        console.log("temp: " + temp);
-        console.log("Country: " + location)
-    })
-    .catch(function(error) {
-    console.log(error)
-})
-
-
-
-
-app.listen(port, () => console.log(`Server running on http://localhost:${port}`))
+    methods: {
+        getTemperature: function(location) {
+            location = location.target.elements.tempHold.value;
+            console.log(location)
+            let tempApi = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=82d42ea6185f3c0018888ea6bc0444e3`;                
+            fetch(tempApi)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    this.temp = data['main']['temp'];
+                })
+                .catch(function(error) {
+                    console.log(error)
+                })
+        }
+    }
+});
---
+++ server.js
@@ -0,0 +1,54 @@
+// libraries
+const express = require('express')
+const fetch = require("node-fetch");
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const port = 8000;
+
+app.use(bodyParser.json());
+app.use(express.urlencoded( { extended: true }));
+
+// routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+//localhost:8000/users/23/books/3422
+app.get('/users/:userId/books/:bookId', function(req, res) {
+    res.send(req.params)
+    console.log(req.params)
+})
+
+
+const api = 'http://api.openweathermap.org/data/2.5/weather?q=Minneapolis&units=imperial&appid=82d42ea6185f3c0018888ea6bc0444e3';
+
+// put inside a function
+fetch(api)
+    .then(response => response.json())
+    .then(data => {
+        var location = data['name']
+        var temp = data['main']['temp']
+        var id = data['id']
+        console.log("temp: " + temp)
+        console.log("Country: " + location)
+        console.log('id: ' + id)
+    })
+    .catch(function(error) {
+    console.log(error)
+})
+
+// function getTemperature(location) {
+//     let tempApi = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=82d42ea6185f3c0018888ea6bc0444e3`;
+
+//     fetch(tempApi)
+//         .then(response => response.json())
+//         .then(data => {
+//             return data['main']['temp'];
+//         })
+//         .catch(function(error) {
+//             console.log(error)
+//         })
+// }
+
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`))
