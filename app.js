ceaf979245d1a02f955dea1d353a17891a3efaf8
Daniel Taufiq <Daniel-Taufiq@users.noreply.github.com>
2020-08-04 15:44:29 -0500

    Update getData
    + Moved fetch() into its own method
    + getData() will be replaced with getData2()

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+//import fetch from "node-fetch";
+
+
 var app = new Vue({
     el: '#app',
     data: {
@@ -5,19 +8,45 @@ var app = new Vue({
     },
 
     methods: {
-        getTemperature: function(location) {
-            location = location.target.elements.tempHold.value;
-            console.log(location)
-            let tempApi = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=82d42ea6185f3c0018888ea6bc0444e3`;                
-            fetch(tempApi)
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data)
-                    this.temp = data['main']['temp'];
-                })
-                .catch(function(error) {
-                    console.log(error)
-                })
+        async getTemperature(location) {
+            try {
+                location = location.target.elements.tempHold.value;
+                console.log(location)
+                let tempApi = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=82d42ea6185f3c0018888ea6bc0444e3`;    
+                
+                const data = await this.getData2(tempApi)
+                this.temp = data['main']['temp']
+            } catch(err) {
+                console.log(err)
+            }
+            
+        },
+         async getData(api) {
+            console.log('before fetch')
+            let response = await fetch(api)
+            console.log(response)
+            let text = await response.json()
+            console.log(text)
+            return text
+        },
+        getData2: async function(api) {
+            try{
+                const response = await fetch(api);
+                if (response.ok) {
+                    let text = await response.json();
+                    console.log(text)
+                    return text
+                }
+            } catch{
+                return Promise.reject(new ResponseError('Invalid JSON: ' + error.message));
+            };
         }
+        // getUnits: function() {
+            
+        // }
+    },
+    
+    computed: {
+
     }
 });
