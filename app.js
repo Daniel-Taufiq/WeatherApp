dddae1054349e22da3eaea002c8e48d3d22001d6
Daniel Taufiq <taufiqdanny@gmail.com>
2021-02-07 13:32:37 -0600

    added weather icons

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
+const skycons = new Skycons({ color: "black" })
 var app = new Vue({
     el: '#app',
     data: {
         cities1: {
-            items: [],
+            items: []
         },
         cities2: {
-            items: [],
+            items: []
+        },
+        icons1: {
+            items: []
+        },
+        icons2: {
+            items: []
         },
         temp: '',
         loc: ''
@@ -17,10 +24,46 @@ var app = new Vue({
         for(let i = 0; i < cities1.length; i++) {
             this.cities1.items.push(await getTemperature(cities1[i]));
             this.cities2.items.push(await getTemperature(cities2[i]));
+            this.icons1.items.push(await getIcons(cities1[i]))
+            this.icons2.items.push(await getIcons(cities2[i]))
+        }
+        this.setWeatherIcon()
+    },
+    methods: {
+        async setWeatherIcon() {
+            //skycons.add('icon1', Skycons.Cloudy)
+            // skycons.play()
+            console.log(this.icons1.items.length)
+            for(let i = 1; i <= this.icons1.items.length; i++) {
+                skycons.add(`icon${i}`, await getSVG(this.icons1.items[i-1]))
+                skycons.add(`icon${3+i}`, await getSVG(this.icons2.items[i-1]))
+                // console.log(`set icon${i} using`, this.icons1.items[i-1])
+                // console.log(`set icon${3+i} using`, this.icons2.items[i-1])
+            }
+            skycons.play()
         }
     }
 });
 
+async function getSVG(icon) {
+    //https://openweathermap.org/weather-conditions
+    //https://codepen.io/palimadra/pen/vfncA
+    if(icon === 'clear sky') {
+        return Skycons.CLEAR_DAY
+    }
+    else if(icon === 'few clouds') {
+        return Skycons.PARTLY_CLOUDY_DAY
+    }
+    else if(icon === 'scattered clouds' || icon === 'broken clouds' || icon == 'overcast clouds') {
+        return Skycons.CLOUDY
+    }
+    else if(icon === 'shower rain' || icon === 'rain' || icon === 'thunderstorm' || icon == 'mist') {
+        return Skycons.RAIN
+    }
+    else if(icon === 'snow') {
+        return Skycons.SNOW
+    }
+}
 async function getTemperature(location) {
     try {
         const tempApi = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=82d42ea6185f3c0018888ea6bc0444e3`;    
@@ -31,11 +74,22 @@ async function getTemperature(location) {
     }            
 }
 
+async function getIcons(location) {
+    try {
+        const tempApi = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=82d42ea6185f3c0018888ea6bc0444e3`;    
+        let data = await this.getData(tempApi)
+        console.log(data['weather'][0]['main'])
+        return data['weather'][0]['description']
+    } catch(err) {
+        console.log(err)
+    }    
+}
+
 async function getData(api) {
     try{
-        console.log('api is ' + api)
+        //console.log('api is ' + api)
         const response = await fetch(api);
-        console.log(response)
+        //console.log(response)
         if (response.ok) {
             const text = await response.json();
             console.log(text)
@@ -45,6 +99,7 @@ async function getData(api) {
         return Promise.reject(new ResponseError('Invalid JSON: ' + error.message));
     };
 }
+
 
 
 
